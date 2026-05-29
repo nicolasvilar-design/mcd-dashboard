118f7dcd077a79cbd149d68f98b379d0546fdf7d
nicolasvilar-design <tu-email@ejemplo.com>
2026-05-29 14:54:45 -0300

    fix: configure storybook with react-webpack5 framework

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react';
 
 const config: StorybookConfig = {
@@ -6,12 +7,12 @@ const config: StorybookConfig = {
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: '@storybook/react',
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {},
   },
   docs: {
@@ -20,3 +21,7 @@ const config: StorybookConfig = {
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
